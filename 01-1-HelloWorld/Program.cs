beb865db8ef7ba92fbf9544976b51d242748fa16
John McMeen <johnmcmeen@gmail.com>
2024-08-28 15:41:41 -0400

    Added Hello World example

---
+++ 01-1-HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _01_1_HelloWorld
+{
+    /// <summary>
+    /// Hello World!
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        /// Program entry point
+        /// </summary>
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

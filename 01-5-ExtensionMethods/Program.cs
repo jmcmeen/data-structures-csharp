cdde640823a4ad8f22ac17372c37af8efab8aaaf
John McMeen <johnmcmeen@gmail.com>
2024-11-21 18:37:24 -0500

    added extension methods example

---
+++ 01-5-ExtensionMethods/Program.cs
@@ -0,0 +1,37 @@
+﻿internal static class Program
+{
+    private static void Main()
+    {
+        const int ITERATIONS = 100;
+
+        List<Thing> things = new List<Thing>();
+
+        for (int i = 0; i < ITERATIONS; i++)
+        {
+            things.Add(new Thing()
+            {
+                Stuff = "Hello World",
+                Byte = (byte)i,
+            });
+        }
+
+        things.ToStringArray();
+
+        things.Average(x => x.Byte);
+    }
+}
+
+public static class Extensions
+{
+    public static string[] ToStringArray(this List<Thing> things)
+    {
+        string[] array = new string[things.Count];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = things[i].ToString();
+        }
+
+        return array;
+    }
+}
---
+++ 01-5-ExtensionMethods/Thing.cs
@@ -0,0 +1,12 @@
+﻿
+public class Thing
+{
+    public string Stuff { get; set; }
+    public byte Byte { get; set; }
+
+    public string ToString()
+    {
+        return $"{Stuff},{Byte}";
+    }
+}
+

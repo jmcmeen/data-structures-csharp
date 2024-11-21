8ec9494c104b15e5a0d9b55ebb0cd039c60bf1f9
John McMeen <johnmcmeen@gmail.com>
2024-11-21 18:46:55 -0500

    statics

---
+++ 01-6-StaticVariables/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        const int ITERATIONS = 100;
+
+        List<Thing> things = new List<Thing>();
+
+        for (int i = 0; i < ITERATIONS; i++)
+        {
+            things.Add(new Thing()
+            {
+                Stuff = "Hello World",
+                Byte = (byte)i,
+            });
+        }
+
+        foreach(Thing thing in things)
+        {
+            Console.WriteLine(thing);
+        }
+    }
+}
+
+public class Thing
+{
+    public string Stuff { get; set; }
+    public byte Byte { get; set; }
+    public int Id { get; set; }
+    public static int NumThings { get; set; }
+
+    public Thing() 
+    {
+        Id = NumThings;
+        NumThings += 1;
+    }
+
+    public override string ToString()
+    {
+        return $"{Id},{Stuff},{Byte},{NumThings}";
+    }
+}
